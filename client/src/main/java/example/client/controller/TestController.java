662c6a5e5c05848f2d8f5671a41da1bd4ea0bdc2
wujiaxing <jiaxing.wu@reach-core.com>
2019-05-30 12:00:12 +0800

    client

---
+++ client/src/main/java/example/client/controller/TestController.java
@@ -0,0 +1,16 @@
+package example.client.controller;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @RequestMapping("/test")
+    public String getCity(@RequestBody String param) {
+        String returnString = "clint param: " + param;
+        System.out.println(returnString);
+        return returnString;
+    }
+}
--- zuul/src/main/java/example/zuul/ZuulApplication.java
+++ zuul/src/main/java/example/zuul/ZuulApplication.java
@@ -2,10 +2,12 @@ package example.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @EnableZuulProxy
 @SpringCloudApplication
+@EnableEurekaClient
 public class ZuulApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ZuulApplication.class, args);
