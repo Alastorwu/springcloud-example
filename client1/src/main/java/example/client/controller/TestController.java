838134944f649cb99b858a1e47d26c7552f5b84d
wujiaxing <jiaxing.wu@reach-core.com>
2019-05-31 15:41:33 +0800

    ribbon

---
+++ client1/src/main/java/example/client/controller/TestController.java
@@ -0,0 +1,23 @@
+package example.client.controller;
+
+import example.client.remote.TestRemote;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+public class TestController {
+
+    @Resource
+    private TestRemote testRemote;
+
+    @RequestMapping(value = "/test",method = RequestMethod.POST)
+    public String getCity(@RequestBody String param) {
+        String returnString = "api1 param: " + testRemote.test(param);
+        System.out.println(returnString);
+        return returnString;
+    }
+}
---
+++ data1/src/main/java/example/data/controller/TestController.java
@@ -0,0 +1,17 @@
+package example.data.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @RequestMapping(value = "/test",method = RequestMethod.POST)
+    public String getCity(@RequestParam("param") String param) {
+        String returnString = "data param: " + param;
+        System.out.println(returnString);
+        return param;
+    }
+}
