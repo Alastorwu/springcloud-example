0571f8bfa6b2abefc2dcbc1b54340a2a0c7a7dc7
wujiaxing <jiaxing.wu@reach-core.com>
2019-06-05 17:23:35 +0800

    test

--- client1/src/main/java/example/client/service/AsyncService.java
+++ client1/src/main/java/example/client/service/AsyncService.java
@@ -21,28 +21,6 @@ public class AsyncService {
         return new AsyncResult<>(testRemote.test(param));
     }
 
-    @Async
-    public Future<String> doTask1() throws InterruptedException{
-        System.out.println("Task1 started.");
-        long start = System.currentTimeMillis();
-        Thread.sleep(5000);
-        long end = System.currentTimeMillis();
-        System.out.println("Task1 finished, time elapsed: {"+(end-start)+"} ms.");
-
-        return new AsyncResult<>("Task1 accomplished!");
-    }
-
-    @Async
-    public Future<String> doTask2() throws InterruptedException{
-        System.out.println("Task2 started.");
-        long start = System.currentTimeMillis();
-        Thread.sleep(3000);
-        long end = System.currentTimeMillis();
-
-        System.out.println("Task2 finished, time elapsed: {"+(end-start)+"} ms.");
-
-        return new AsyncResult<>("Task2 accomplished!");
-    }
 
 
 }
